17d5f015ee9bb845e76f127d6e2456d281b775af
thalha-dev <dev.thalha@gmail.com>
2024-01-27 01:04:10 +0530

    dotnet webapi course 1 complete

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
+using HPlusSport.API.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// builder.Services.AddControllers()
+//   .ConfigureApiBehaviorOptions(options =>
+//   {
+//       options.SuppressModelStateInvalidFilter = true;
+//   });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ShopContext>(options => options.UseInMemoryDatabase("Shop"));
 
 var app = builder.Build();
 
