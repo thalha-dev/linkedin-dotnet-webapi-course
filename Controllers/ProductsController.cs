36edc357d33fb0f43fef811b29c9b2424ccd9c48
thalha-dev <dev.thalha@gmail.com>
2024-01-27 19:36:23 +0530

    http header versioning

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -7,7 +7,8 @@ namespace HPlusSport.API.Controllers
 {
     [ApiVersion("1.0")]
     // [Route("api/[controller]")]
-    [Route("v{v:apiVersion}/products")]
+    // [Route("v{v:apiVersion}/products")]
+    [Route("products")]
     [ApiController]
     public class ProductsV1Controller : ControllerBase
     {
@@ -171,7 +172,8 @@ namespace HPlusSport.API.Controllers
     }
 
     [ApiVersion("2.0")]
-    [Route("v{v:apiVersion}/products")]
+    // [Route("v{v:apiVersion}/products")]
+    [Route("products")]
     [ApiController]
     public class ProductsV2Controller : ControllerBase
     {
